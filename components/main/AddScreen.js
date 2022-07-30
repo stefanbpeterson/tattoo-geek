ac4aa522a4240b836f047ab888de6edc97066306
Stefan Peterson <stefanbpeterson@gmail.com>
2022-07-29 22:36:30 -0600

    adding camera functions

--- components/main/AddScreen.js
+++ components/main/AddScreen.js
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
-export default function Add() {
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [camera, setCamera] = useState(null)
+  const [type, setType] = useState(CameraType.back)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async() => {
+    if(camera) {
+      const data = await camera.takePictureAsync(null)
+      console.log(data.uri)
+    }
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View>
-      <Text>Add</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+        ref={ref => setCamera(ref)} 
+        style={styles.fixedRatio} 
+        type={type} 
+        ratio={'1:1'} />
+      </View>
+      <Button
+      title='Flip Image'
+      onPress={() => {
+        setType(type === CameraType.back ? CameraType.front : CameraType.back);
+      }}>
+      </Button>
+      <Button 
+      title='Take Picture'
+      onPress={() => takePicture()}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1
+  }
+})
